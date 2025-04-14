78535ee73937d1b005c8f5ef01fe554db15feae1
0031432412027 <0031432412027@LTI2202>
2025-04-14 14:26:12 -0300

    Adicionar arquivos de projeto.

---
+++ Adaptadores.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_14_04_25
+{
+    //internal class Adaptadores
+    //{
+    //}
+
+    // Interface alvo
+    public interface Poligono
+    {
+        double getArea();
+    }
+
+    // Classe Quadrado
+    public class Quadrado : Poligono
+    {
+        private double lado;
+
+        public Quadrado(double lado)
+        {
+            this.lado = lado;
+        }
+
+        public double getArea()
+        {
+            return lado * lado;
+        }
+    }
+
+    // Classe Triangulo
+    public class Triangulo : Poligono
+    {
+        private double baseTriangulo;
+        private double altura;
+
+        public Triangulo(double baseTriangulo, double altura)
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.altura = altura;
+        }
+
+        public double getArea()
+        {
+            return (baseTriangulo * altura) / 2;
+        }
+    }
+
+    // Classe estrangeira que precisa ser adaptada
+    public class HexagonoEstrangeiro
+    {
+        private double lado;
+
+        public HexagonoEstrangeiro(double lado)
+        {
+            this.lado = lado;
+        }
+
+        public float calculaArea()
+        {
+            return (float)((3 * (lado * lado) * Math.Sqrt(3)) / 2);
+        }
+    }
+
+    // Adaptador para HexagonoEstrangeiro
+    public class AdaptadorHexagono : Poligono
+    {
+        private HexagonoEstrangeiro hexagono;
+        public AdaptadorHexagono(HexagonoEstrangeiro hexagono)
+        {
+            this.hexagono = hexagono;
+        }
+        public double getArea()
+        {
+            // Converte o resultado de float para double
+            return (double)hexagono.calculaArea();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Adapter_14_04_25;
+
+List<Poligono> polígonos = new List<Poligono>();
+
+// Adicionando polígonos à lista
+polígonos.Add(new Quadrado(5.0));       // Área: 25
+polígonos.Add(new Triangulo(4.0, 3.0)); // Área: 6
+                                        // Criando e adaptando o hexágono estrangeiro
+HexagonoEstrangeiro hexEstrangeiro = new HexagonoEstrangeiro(2.0); // Área: ~10.3923
+polígonos.Add(new AdaptadorHexagono(hexEstrangeiro));
+
+// Calculando e exibindo áreas de forma polimórfica
+foreach (var poligono in polígonos)
+{
+    Console.WriteLine($"Área do polígono: {poligono.getArea():F4}");
+}
+
+/*
+Saída esperada:
+Área do polígono: 25,0000
+Área do polígono: 6,0000
+Área do polígono: 10,3923
+*/
